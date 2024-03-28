e19589045c3c9b47166d992fda43bc010cb20c66
anamm8 <108732181+anamm8@users.noreply.github.com>
2024-03-28 20:05:06 +0000

    Update main.py
    Day 1 - print

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
+# Day 1 - Print
+
+#Printing strings !Attention to use exactly the predefined keywords. Print("a") won't work!
+print("Hello World")
+print("this will appear on a new line")
+print("yet another line")
+
+#Printing exactly as I want
+print("""
+          This is
+                  an
+      experiment
+""")
+
+# Input
+# very similar to the print command, except that it'll show the message in the console then wait until the user has typed something into the console and pressed enter
+input("Input >")
 
